f58d9701d56f43bdc5464fbc9c2d8da8f97de671
dhoonjang <dhoonjang@gmail.com>
2018-09-15 03:26:39 +0900

    floor23 4.73

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -5,32 +5,42 @@ const photoDB = [
   {
     id : 0,
     name : "첫번째 사진",
-    src : require('img/photo/photo1.png')
+    src : require('img/photo/sphoto1.jpg')
   },
   {
     id : 1,
     name : "두번째 사진",
-    src : require('img/photo/photo2.png')
+    src : require('img/photo/sphoto2.png')
   },
   {
     id : 2,
     name : "세번째 사진",
-    src : require('img/photo/photo3.png')
+    src : require('img/photo/sphoto3.png')
   },
   {
     id : 3,
     name : "네번째 사진",
-    src : require('img/photo/photo4.jpg')
+    src : require('img/photo/sphoto4.png')
   },
   {
     id: 4,
     name : "다섯번째 사진",
-    src : require('img/photo/photo5.png')
+    src : require('img/photo/sphoto5.png')
   },
   {
     id: 5,
     name : "여섯번째 사진",
-    src : require('img/photo/photo6.png')
+    src : require('img/photo/sphoto6.png')
+  },
+  {
+    id: 6,
+    name : "일곱번째 사진",
+    src : require('img/photo/sphoto7.jpg')
+  },
+  {
+    id: 7,
+    name : "여덟번째 사진",
+    src : require('img/photo/sphoto8.png')
   }
 ]
 
